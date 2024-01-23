c38769727acc012b261c75e4bd78bef503f7b571
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2024-01-23 13:38:58 +0200

    First commint

---
+++ src/FactoryMethod/Main.java
@@ -0,0 +1,9 @@
+package FactoryMethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AterioivaOtus opettaja = new Opettaja();
+        opettaja.aterioi();
+    }
+}
